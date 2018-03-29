639be10b9df02b80ac14f5c75188cf75c2581630
zhangheng <zhangheng2683@gmail.com>
2018-03-29 18:09:23 +0800

    create_samplestr.sql

---
+++ create_samplestr.sql
@@ -0,0 +1,21 @@
+create table  samplestr(
+    str1 varchar(40),
+    str2 varchar(40),
+    str3 varchar(40)
+);
+
+start transaction;
+insert  into samplestr values('opx','rt',null);
+insert  into samplestr values('abc','def',null);
+insert  into samplestr values('');
+insert  into samplestr values();
+insert  into samplestr values();
+insert  into samplestr values();
+insert  into samplestr values();
+insert  into samplestr values();
+insert  into samplestr values();
+insert  into samplestr values();
+insert  into samplestr values();
+insert  into samplestr values();
+insert  into samplestr values();
+insert  into samplestr values();
