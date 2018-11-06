8d4d1074f59429342b01138ed1705c7229794822
zhangheng <zhangheng2683@gmail.com>
2018-11-07 00:22:48 +0800

    修改表结构

--- sample/post_like.sql
+++ sample/post_like.sql
@@ -2,7 +2,8 @@ drop table if exists wechat_like;
 create table wechat_like(
        id varchar(40),
        `type` integer not null,
-       postOrCommentId varchar(40) not null,
+       postId varchar(40) not null,
+       commentId varchar(40) not null,
        creatorId varchar(40) not null,
        createTime bigint not null,
        `delete` integer not null,
